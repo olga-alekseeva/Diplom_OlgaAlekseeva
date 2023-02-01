2aeb93b02d223f9b09b52bc925bfab357a10933e
olga-alekseeva <oliasca@gmail.com>
2023-02-01 14:44:23 +0300

    задание 1

--- Assets/_Root/Scripts/Play Fab/PlayFabLogin.cs
+++ Assets/_Root/Scripts/Play Fab/PlayFabLogin.cs
@@ -1,11 +1,39 @@
 using PlayFab;
 using PlayFab.ClientModels;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class PlayFabLogin : MonoBehaviour
+
 {
+    [SerializeField] public Button loginButton;
+    [SerializeField] public Text failAnswer;
+    [SerializeField] public Text successAnswer;
+   
     public void Start()
     {
+        loginButton.onClick.AddListener(() => Login());
+    }
+   
+    private void OnLoginSuccess(LoginResult result)
+    {
+        Debug.Log("Congratulations, you made successfulAPI call!");
+        successAnswer.text = "Congratulations, you made successfulAPI call!";
+        successAnswer.color = Color.green;
+    }
+    private void OnLoginFailure(PlayFabError error)
+    {
+        var errorMessage = error.GenerateErrorReport();
+        Debug.LogError($"Something went wrong:{errorMessage}");
+        failAnswer.text = errorMessage;
+        failAnswer.color = Color.red;
+    }
+
+    private void Login()
+    {
+
         // Here we need to check whether TitleId propertyis configured
         // in settings or not
         if (string.IsNullOrEmpty(PlayFabSettings.staticSettings.TitleId))
@@ -16,14 +44,5 @@ public class PlayFabLogin : MonoBehaviour
         }
         var request = new LoginWithCustomIDRequest { CustomId = "GeekBrainsLesson3", CreateAccount = true };
         PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure);
-    }
-    private void OnLoginSuccess(LoginResult result)
-    {
-        Debug.Log("Congratulations, you made successfulAPI call!");
-    }
-    private void OnLoginFailure(PlayFabError error)
-    {
-        var errorMessage = error.GenerateErrorReport();
-        Debug.LogError($"Something went wrong:{errorMessage}");
     }
 }
