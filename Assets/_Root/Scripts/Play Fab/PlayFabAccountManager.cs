7af5f0bd3c554388bd72ff5dee8681facc66757f
olga-alekseeva <oliasca@gmail.com>
2023-02-10 14:36:22 +0300

    Изменила UI на сцене Game
    Сделала кнопку чтобы убирать приветственный экран,
    добавила канвас для инвентаря

--- Assets/_Root/Scripts/Play Fab/PlayFabAccountManager.cs
+++ Assets/_Root/Scripts/Play Fab/PlayFabAccountManager.cs
@@ -3,14 +3,25 @@ using PlayFab.ClientModels;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
+
 internal sealed class PlayFabAccountManager : MonoBehaviour
 {
     [SerializeField] private TMP_Text _titleLabel;
+    [SerializeField] private Button _okButton;
+    [SerializeField] private Canvas _greetingCanvas;
     private void Start()
     {
         PlayFabClientAPI.GetAccountInfo(new GetAccountInfoRequest(),
         OnGetAccountSuccess, OnFailure);
+        _okButton.onClick.AddListener(ExitGreetingCanvas);
     }
+
+    private void ExitGreetingCanvas()
+    {
+        _greetingCanvas.gameObject.SetActive(false);
+    }
+
     private void OnGetAccountSuccess(GetAccountInfoResult result)
     {
         _titleLabel.text = $"Welcome back, Player ID {result.AccountInfo.PlayFabId}, Player accout {result.AccountInfo.Username}";
